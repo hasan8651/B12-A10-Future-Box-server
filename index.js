a81628d254c4328af75f4fd850e626be3ffe78de
hasan8651 <hasan865@gmail.com>
2025-11-08 19:36:44 +0600

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
-
 // middleware
 app.use(cors());
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbonu5x.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Future-Box server is running')
 })
 
+async function run() {
+    try {
+        await client.connect();
 
+        const db = client.db('hasan865_db');
+        
 
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log(`Future-Box server is running on port: ${port}`)
