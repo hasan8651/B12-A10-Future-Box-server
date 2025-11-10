2562eedbb430b8bae075dc0fe0bd1e94f9334da8
hasan8651 <hasan865@gmail.com>
2025-11-11 01:06:41 +0600

    Implement Delete Course Functionality

--- index.js
+++ index.js
@@ -65,6 +65,15 @@ async function run() {
       res.send(result);
     });
 
+
+    app.delete('/courses/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coursesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
